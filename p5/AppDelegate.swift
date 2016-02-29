c4853e9b35e9f44b26b17a5ee0a855b89dcb58e5
hunglun <hunglun.o@gmail.com>
2016-02-29 21:28:43 +0800

    read custom parameters passed to guitarbuddy://

--- p5/AppDelegate.swift
+++ p5/AppDelegate.swift
@@ -14,6 +14,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+        print("Calling Application Bundle ID: \(sourceApplication)")
+        print("URL scheme: \(url)")
+//        print("URL query: ", [url query]);
+        
+        return true
+    }
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
